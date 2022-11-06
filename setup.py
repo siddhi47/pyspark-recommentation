fbb207e97c3536c75c5e74f922be2358399ab559
Siddhi <siddhi.47.skb@gmail.com>
2022-11-06 16:34:56 -0600

    First Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyspark_recommendation_system',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pyspark',
+        ],
+    description='Recommendation systems using pyspark',
+    authors =['Gaurab Subedi', 'Siddhi Kiran Bajracharya', 'Unish Rarnikar'],
+    )
+
